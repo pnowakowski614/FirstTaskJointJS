93b5e771eee3bb056e47aa5b8aaf85236c32fa04
pnowakowski614 <p.nowakowski416@gmail.com>
2022-05-31 23:33:36 +0200

    feat(): add event on every click

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+const randomColor = () => {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+const randomBorderSize = () => {
+    return Math.floor(Math.random()*40);
+}
+
+const randomCircleSize = () => {
+    return Math.floor(Math.random()*200) + 100;
+}
+
 const namespace = joint.shapes;
+
+const defaultAttr = {
+    backgroundFill: 'white',
+    fontSize: 20,
+    labelFill: 'black',
+};
 
 const graph = new joint.dia.Graph({}, { cellNamespace: namespace });
 
@@ -20,13 +37,13 @@ rect.position(240, 270);
 rect.resize(120, 100);
 rect.attr({
     body: {
-        fill: 'white',
-        event: 'element:background-change'
+        fill: defaultAttr.backgroundFill,
+        event: 'element:background-change',
     },
     label: {
         text: 'Rectangle',
-        fontSize: 20,
-        fill: 'black'
+        fontSize: defaultAttr.fontSize,
+        fill: defaultAttr.labelFill
     }
 });
 rect.addTo(graph);
@@ -36,29 +53,31 @@ circle.position(450, 270);
 circle.resize(100, 100);
 circle.attr({
     body: {
-        fill: 'white',
+        fill: defaultAttr.backgroundFill,
         event: 'element:resize'
     },
     label: {
         text: 'Circle',
-        fontSize: 20,
-        fill: 'black'
+        fontSize: defaultAttr.fontSize,
+        fill: defaultAttr.labelFill
     }
 });
 circle.addTo(graph);
+
+
 
 const ellipse = new joint.shapes.standard.Ellipse();
 ellipse.position(630, 270);
 ellipse.resize(200, 100);
 ellipse.attr({
     body: {
-        fill: 'white',
+        fill: defaultAttr.backgroundFill,
         event: 'element:label-change'
     },
     label: {
         text: 'Ellipse',
-        fontSize: 20,
-        fill: 'black'
+        fontSize: defaultAttr.fontSize,
+        fill: defaultAttr.labelFill
     }
 });
 ellipse.addTo(graph);
@@ -68,31 +87,31 @@ polygon.position(930, 270);
 polygon.resize(100, 100);
 polygon.attr({
     body: {
-        fill: 'white',
+        fill: defaultAttr.backgroundFill,
         event: 'element:stroke-change'
     },
     label: {
         text: 'Polygon',
-        fontSize: 20,
-        fill: 'black'
+        fontSize: defaultAttr.fontSize,
+        fill: defaultAttr.labelFill
     }
 });
 polygon.addTo(graph);
 
 paper.on('element:background-change', (elementView) => {
-   elementView.model.attr('body/fill', randomColor);
+   elementView.model.attr('body/fill', randomColor());
 });
  
 paper.on('element:label-change', (elementView) => {
-   elementView.model.attr('label/fill', randomColor);
+   elementView.model.attr('label/fill', randomColor());
 });
  
 paper.on('element:resize', (elementView) => {
-   elementView.model.resize(150, 150);
+   elementView.model.resize(randomCircleSize(), randomCircleSize());
 });
  
 paper.on('element:stroke-change', (elementView) => {
-   elementView.model.attr('body/stroke', randomColor);
-   elementView.model.attr('body/strokeWidth', 8);
+   elementView.model.attr('body/stroke', randomColor());
+   elementView.model.attr('body/strokeWidth', randomBorderSize());
 });
 
